3f830fb7c8a4e1c857f98fc2fb6924939f9d8701
Aleksey <Aleksey.bortnik91@gmail.com>
2021-07-05 22:12:52 +0300

    fixed bugs in styles selectors, html classes, and naming.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const sourcemaps = require("gulp-sourcemaps");
 const watch = require("gulp-watch");
 
 gulp.task("sass-compile", function() {
-  return gulp.src("./source/scss/**/*.scss")
+  return gulp.src("./scss/**/*.scss")
     .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
     .pipe(sourcemaps.write("./"))
@@ -12,5 +12,5 @@ gulp.task("sass-compile", function() {
 });
 
 gulp.task("watch", function() {
-  gulp.watch("./source/scss/**/*.scss", gulp.series("sass-compile"));
+  gulp.watch("./scss/**/*.scss", gulp.series("sass-compile"));
 });
