cb1ea550feb3573c026312f6aa761c361f8962cb
Aleksey <Aleksey.bortnik91@gmail.com>
2021-07-08 14:23:27 +0300

    Common styles are split into two files. Bugs fixed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require('sass'));
-const sourcemaps = require("gulp-sourcemaps");
-const watch = require("gulp-watch");
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const watch = require('gulp-watch');
 
-gulp.task("sass-compile", function() {
-  return gulp.src("./scss/**/*.scss")
+gulp.task('sass-compile', function() {
+  return gulp.src('./scss/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass().on("error", sass.logError))
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest("./dist/"));
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task("watch", function() {
-  gulp.watch("./scss/**/*.scss", gulp.series("sass-compile"));
+gulp.task('watch', function() {
+  gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'));
 });
